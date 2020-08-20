539e208f511dbd041753b8e220142f391720bbe2
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2020-08-21 04:11:55 +0700

    Add simple-demo, entrypoint-and-cmd-demo, stack-demo etc...

---
+++ simple-demo/echo-name.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo supparurk
+echo Kera
+echo John Nonphala
+echo Zygen group
+echo Kera
+echo Pae
+echo Benznaja
+echo YoungNuay
+echo Ahihi
+echo Kittiew
+echo InFamous
+echo King
+echo tonglee
+echo Pae2
+echo tunoat
+echo aofdemo3
+echo Pae3
+echo Pae4
+echo windwinn
+echo iceweez
+echo tong
+echo ployue
