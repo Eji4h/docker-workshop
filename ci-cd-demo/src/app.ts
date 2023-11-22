f12847805ff0576f1161e5da32e72c6e0acc36be
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2023-11-22 21:59:05 +0700

    feat: 🎸 add ci-cd-demo

---
+++ ci-cd-demo/src/app.ts
@@ -0,0 +1,23 @@
+import cors from 'cors';
+import express from 'express';
+
+import { port } from './config.env';
+
+const main = async () => {
+  const app = express();
+  const router = express.Router();
+  app.use(cors());
+  app.use('/api/v1', router);
+
+  router.get('/', (_, res) => {
+    const response = 'Zero to Hero : Docker for IT / Software Developer';
+    res.send(response);
+  });
+
+
+  app.listen(port, () => {
+    console.log(`server is listening on ${port}`);
+  });
+};
+
+main();
